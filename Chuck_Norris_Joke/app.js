c526311bb48d2643934473a7bbdf5ce22b31aad6
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-09-20 11:13:59 +0100

    Getting the number of jokes

---
+++ Chuck_Norris_Joke/app.js
@@ -0,0 +1,9 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+    const number = document.querySelector('input[type="number"]').value;
+
+
+
+    e.preventDefault();
+}
